47b6cc125eee9554f0e1a210e15068e1c67b36b6
Alexandre Nguyen <alex.nr@hotmail.fr>
2017-05-05 16:49:51 +0200

    Used custom parameters to build and upload AMI.

--- provision.sh
+++ provision.sh
@@ -10,10 +10,4 @@ sudo apt-get -y update
 sudo apt-get -y install ansible
 
 # Install AMI Tools.
-sudo apt-get install -y ruby unzip
-wget https://s3.amazonaws.com/ec2-downloads/ec2-ami-tools.zip
-sudo mkdir -p /usr/local/ec2
-sudo unzip ec2-ami-tools.zip -d /usr/local/ec2
-export EC2_AMITOOL_HOME=/usr/local/ec2/`ls /usr/local/ec2 | grep ec2-ami-tools | grep -v zip`/bin
-echo "EC2_AMITOOL_HOME="$EC2_AMITOOL_HOME
-export PATH=$EC2_AMITOOL_HOME+":"+$PATH
+sudo apt-get install -y ec2-ami-tools
